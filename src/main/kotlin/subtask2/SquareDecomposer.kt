d8fdabbfe837bb383876c511608da4b7f9b63f90
Grechotg <godster900@gmail.com>
2021-07-12 14:03:15 +0300

    2 done

--- src/main/kotlin/subtask2/SquareDecomposer.kt
+++ src/main/kotlin/subtask2/SquareDecomposer.kt
@@ -4,6 +4,29 @@ class SquareDecomposer {
 
     // TODO: Complete the following function
     fun decomposeNumber(number: Int): Array<Int>? {
-        throw NotImplementedError("Not implemented")
+        val newLong = number.toLong()
+        val result = mutableListOf(newLong)
+        var x = 0L
+
+        while (!result.isNullOrEmpty()) {
+            val current = result.last()
+            result.indexOfLast {
+                result.remove(it)
+            }
+
+            x += current*current
+            for (i in current - 1 downTo 0) {
+                if (x - i*i >= 0) {
+                    x -= i*i
+                    result.add(i)
+
+                    if (x == 0L) {
+                        result.sort()
+                        return result.map { it.toInt() }.toTypedArray()
+                    }
+                }
+            }
+        }
+        return null
     }
 }
